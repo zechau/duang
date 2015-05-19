f498150e8b6fda1e640ecabb60581874596ed6f6
unknown <huangzhichao@eccyf-huangzhc.ecc.360buyAD.local>
2015-05-19 14:43:01 +0800

    duang by css3

---
+++ src/duang.js
@@ -0,0 +1,21 @@
+﻿(function(global){
+	var playing = false;
+	var oldPosition = "";
+	var clearAnimatation = function(element){
+		setTimeout(function(){
+			element.className = element.className.replace(/\bduang\b/, "");
+			playing = false;
+			element.style.position = oldPosition;
+		}, 500);
+	}
+	
+	global.duang = function(element){
+		oldPosition = element.style.position;
+		element.style.position = "relative";
+		if(playing === true){
+				return;
+		}
+		element.className += " duang";
+		clearAnimatation(element);
+	}
+})(window);
